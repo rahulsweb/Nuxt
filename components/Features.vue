d23bf3e38ff8893b75bad68080a9beb1ea386bd4
Rahul <rahulsonawanen@gmail.com>
2021-11-19 21:03:18 +0530

    added carousol effect

--- components/Features.vue
+++ components/Features.vue
@@ -1,18 +1,15 @@
 <template>
   <section class="service-one" id="features">
-    <br><br><br>
+    <br /><br /><br />
     <div class="container pt-5">
-      <div class="block-title text-center " >
+      <div class="block-title text-center">
         <h2 class="block-title__title pt-5">Check out our features.</h2>
         <!-- /.block-title__title -->
       </div>
-      <!-- /.block-title -->
-      <div class="row">
-        <div
-          class="col-lg-3 col-md-6 col-sm-12 col-xs-12 wow fadeInUp"
-        
-        >
-          <div class="service-one__single text-center ">
+      <VueSlickCarousel v-bind="settings">
+        <!-- first row -->
+        <div class="p-2">
+          <div class="service-one__single text-center wow fadeInUp">
             <div class="service-one__inner first-block">
               <i class="service-one__icon dimon-icon-laptop"></i>
               <h3>
@@ -25,19 +22,14 @@
                 Send guests your Bip it link or embed it on your website, email
                 campaigns, social profiles, and landing pages.
               </p>
-        
             </div>
             <!-- /.service-one__inner -->
           </div>
           <!-- /.service-one__single -->
         </div>
-        <!-- /.col-lg-3 col-md-6 col-sm-12 -->
-        <div
-          class="col-lg-3 col-md-6 col-sm-12 wow fadeInDown"
-          data-wow-duration="1500ms"
-        >
-          <div class="service-one__single text-center">
-            <div class="service-one__inner sec-block">
+        <div class="p-2">
+          <div class="service-one__single text-center wow fadeInUp">
+            <div class="service-one__inner first-block">
               <i class="service-one__icon dimon-icon-presentation"></i>
               <h3>
                 <a href="#"
@@ -49,19 +41,17 @@
                 Embed Bip it video conferencing solution directly on your
                 website for smoother appointments.
               </p>
-            
             </div>
             <!-- /.service-one__inner -->
           </div>
           <!-- /.service-one__single -->
         </div>
-        <!-- /.col-lg-3 col-md-6 col-sm-12 -->
-        <div
-          class="col-lg-3 col-md-6 col-sm-12 wow fadeInUp "
-          data-wow-duration="1500ms"
-        >
-          <div class="service-one__single text-center ">
-            <div class="service-one__inner third-block">
+        <div class="p-2">
+          <div
+            class="service-one__single text-center wow fadeInUp"
+            data-wow-duration="1500ms"
+          >
+            <div class="service-one__inner first-block">
               <i class="service-one__icon dimon-icon-target"></i>
               <h3>
                 <a href="#"
@@ -73,19 +63,17 @@
                 Set the right form for the right goal: one-on-one, group
                 meetings, and round- robin for rotating availability.
               </p>
-           
             </div>
             <!-- /.service-one__inner -->
           </div>
           <!-- /.service-one__single -->
         </div>
-        <!-- /.col-lg-3 col-md-6 col-sm-12 -->
-        <div
-          class="col-lg-3 col-md-6 col-sm-12 wow fadeInDown"
-          data-wow-duration="1500ms"
-        >
-          <div class="service-one__single text-center">
-            <div class="service-one__inner forth-block">
+        <div class="p-2">
+          <div
+            class="service-one__single text-center wow fadeInUp"
+            data-wow-duration="1500ms"
+          >
+            <div class="service-one__inner first-block">
               <i class="service-one__icon dimon-icon-visualization"></i>
               <h3>
                 <a href="#"
@@ -98,46 +86,255 @@
                 can take the payment at the time of booking, during or at the
                 end of the meeting.
               </p>
-        
             </div>
             <!-- /.service-one__inner -->
           </div>
           <!-- /.service-one__single -->
         </div>
-        <!-- /.col-lg-3 col-md-6 col-sm-12 -->
-      </div>
-      <!-- /.row -->
+        <!-- /.block-title -->
+
+        <!-- /.row -->
+        <!-- first row end -->
+
+        <!-- second row start -->
+
+        <div class="p-2">
+          <div
+            class="service-one__single text-center wow fadeInUp"
+            data-wow-duration="1500ms"
+          >
+            <div class="service-one__inner first-block">
+              <i class="service-one__icon dimon-icon-laptop"></i>
+              <h3>
+                <a href="#"> MULTI-LANGUAGE </a>
+              </h3>
+              <p class="p-2">Bip it speaks yours and your guests’ language</p>
+            </div>
+            <!-- /.service-one__inner -->
+          </div>
+          <!-- /.service-one__single -->
+        </div>
+        <div class="p-2">
+          <div class="service-one__single text-center">
+            <div class="service-one__inner first-block">
+              <i class="service-one__icon dimon-icon-presentation"></i>
+              <h3>
+                <a href="#"> API SUPPORT </a>
+              </h3>
+              <p class="p-2">
+                Are our customized solutions not enough for you? With our APIs,
+                there is no limit to how you want to integrate Bip it within
+                your existing solution.
+              </p>
+            </div>
+            <!-- /.service-one__inner -->
+          </div>
+          <!-- /.service-one__single -->
+        </div>
+        <div class="p-2">
+          <div class="service-one__single text-center">
+            <div class="service-one__inner first-block">
+              <i class="service-one__icon dimon-icon-target"></i>
+              <h3>
+                <a href="#"> WHITE LABEL SUPPORT </a>
+              </h3>
+              <p class="p-2">
+                Our specialized staff is able to create specific booking
+                products for your needs.
+              </p>
+            </div>
+            <!-- /.service-one__inner -->
+          </div>
+          <!-- /.service-one__single -->
+        </div>
+        <div class="p-2">
+          <div class="service-one__single text-center">
+            <div class="service-one__inner first-block">
+              <i class="service-one__icon dimon-icon-visualization"></i>
+              <h3>
+                <a href="#"> WEBINAR SUPPORT </a>
+              </h3>
+              <p class="p-2">
+                Host your large meetings and get supported by our specialists.
+              </p>
+            </div>
+            <!-- /.service-one__inner -->
+          </div>
+          <!-- /.service-one__single -->
+        </div>
+
+        <!-- second row end -->
+
+        <!-- third row start -->
+        <div class="p-2">
+          <div class="service-one__single text-center">
+            <div class="service-one__inner first-block">
+              <i class="service-one__icon dimon-icon-laptop"></i>
+              <h3>
+                <a href="#">REPORTING & ANALYTICS</a>
+              </h3>
+              <p class="p-2">
+                Bip it dashboard allows you to keep an eye on your meetings and
+                your teams’ meetings, on the accountability and the invoicing.
+              </p>
+            </div>
+            <!-- /.service-one__inner -->
+          </div>
+          <!-- /.service-one__single -->
+        </div>
+        <div class="p-2">
+          <div class="service-one__single text-center">
+            <div class="service-one__inner first-block">
+              <i class="service-one__icon dimon-icon-presentation"></i>
+              <h3>
+                <a href="#">POOLED AVAILABILITY FOR TEAMS</a>
+              </h3>
+              <p class="p-2">
+                When you need more staff to meet your customers' requests, Bip
+                it automatically distributes them among the allocated team.
+              </p>
+            </div>
+            <!-- /.service-one__inner -->
+          </div>
+          <!-- /.service-one__single -->
+        </div>
+        <div class="p-2">
+          <div class="service-one__single text-center">
+            <div class="service-one__inner first-block">
+              <i class="service-one__icon dimon-icon-target"></i>
+              <h3>
+                <a href="#">ON-LINE & ON-SITE MEETINGS</a>
+              </h3>
+              <p class="p-2">
+                Meet your invitee wherever they are by providing multiple
+                in-person and virtual location options.
+              </p>
+            </div>
+            <!-- /.service-one__inner -->
+          </div>
+          <!-- /.service-one__single -->
+        </div>
+        <div class="p-2">
+          <div class="service-one__single text-center">
+            <div class="service-one__inner first-block">
+              <i class="service-one__icon dimon-icon-visualization"></i>
+              <h3>
+                <a href="#">CERTIFIED VOTING</a>
+              </h3>
+              <p class="p-2">
+                Bip it supports large meetings and eVoting to make your
+                decision-making process modern, secure and effective.
+              </p>
+            </div>
+            <!-- /.service-one__inner -->
+          </div>
+          <!-- /.service-one__single -->
+        </div>
+        <!-- third row end -->
+
+        <!-- fourth row start -->
+        <div class="p-2">
+          <div class="service-one__single text-center">
+            <div class="service-one__inner first-block">
+              <i class="service-one__icon dimon-icon-laptop"></i>
+              <h3>
+                <a href="#">CALENDARS SYNCHRONIZATION</a>
+              </h3>
+              <p class="p-2">
+                Bip it connects to all your calendars, without conflicts, so
+                you’re never double- booked.
+              </p>
+            </div>
+            <!-- /.service-one__inner -->
+          </div>
+          <!-- /.service-one__single -->
+        </div>
+        <div class="p-2">
+          <div class="service-one__single text-center">
+            <div class="service-one__inner first-block">
+              <i class="service-one__icon dimon-icon-presentation"></i>
+              <h3>
+                <a href="#">FANCY CALENDAR LINK</a>
+              </h3>
+              <p class="p-2">A link that looks good and easy to remember.</p>
+            </div>
+            <!-- /.service-one__inner -->
+          </div>
+          <!-- /.service-one__single -->
+        </div>
+        <div class="p-2">
+          <div class="service-one__single text-center">
+            <div class="service-one__inner first-block">
+              <i class="service-one__icon dimon-icon-target"></i>
+              <h3>
+                <a href="#">REMINDERS MEETINGS</a>
+              </h3>
+              <p class="p-2">
+                Automate reminders - by SMS and email - and follow- ups so your
+                team and your clients won’t miss a meeting.
+              </p>
+            </div>
+            <!-- /.service-one__inner -->
+          </div>
+          <!-- /.service-one__single -->
+        </div>
+        <div class="p-2">
+          <div class="service-one__single text-center">
+            <div class="service-one__inner first-block">
+              <i class="service-one__icon dimon-icon-visualization"></i>
+              <h3>
+                <a href="#">VIDEO CALL ROUTING & FORWARDING</a>
+              </h3>
+              <p class="p-2">
+                Automatically queue and distribute incoming video calls based on
+                a predefined set of rules and criteria.
+              </p>
+            </div>
+            <!-- /.service-one__inner -->
+          </div>
+          <!-- /.service-one__single -->
+        </div>
+        <!-- fourth row end -->
+      </VueSlickCarousel>
     </div>
     <!-- /.container -->
   </section>
 </template>
 
 <script>
+import VueSlickCarousel from "vue-slick-carousel";
+import "vue-slick-carousel/dist/vue-slick-carousel.css";
+// optional style for arrows & dots
+import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
 export default {
   name: "Featires",
+  components: { VueSlickCarousel },
+  data() {
+    return {
+      settings: {
+        arrows: false,
+        dots: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 2000,
+        cssEase: "linear",
+      },
+    };
+  },
 };
 </script>
 
 <style scoped>
-.service-one__single::before{
-  opacity: 1 !important;
+.service-one__single::before {
+  /* opacity: 1 !important; */
 }
 /* .service-one__single{
   height: 590px;
 } */
-.first-block{
-  padding-bottom:20% !important ;
+.first-block {
+  height: 500px;
 }
-.sec-block{
-  padding-bottom:33% !important ;
-}
-.third-block{
-  padding-bottom:19% !important ;
-}
-.forth-block{
-  padding-bottom:5% !important ;
-}
-
-
-
 </style>
